482a39ea6a08218f33f875f6b629e5fab7437c88
scottshadow56 <rudaseswascottmc@gmail.com>
2025-09-14 00:57:11 +0200

    railway stuff

--- vite.config.js
+++ vite.config.js
@@ -12,8 +12,7 @@ export default defineConfig({
   server: {
     host: '0.0.0.0', // This is often necessary for Replit to work correctly
     allowedHosts: [
-      '4bbd29e2-efde-47dc-8b80-4d60d1f81038-00-265c14xc55rjy.riker.replit.dev',
-      // You can add more hosts here if needed
+      'quad-box-production.up.railway.app'
     ]
   }
 })
